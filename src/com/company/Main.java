2501913517acb03714b32a5929599bb90e06b432
MGreco2112 <mgreco2112@tutanota.com>
2021-08-12 18:17:27 -0400

    began testing charCode for cypher

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void atBashCypher(String input) {
+        System.out.println(input.codePointAt(0));
+
+        int testInt = input.codePointAt(0);
+
 
     }
 
     public static void main(String[] args) {
-        System.out.println(MultiplesArray.multiplesArray(10, 5));
+//        System.out.println(MultiplesArray.multiplesArray(10, 5));
 //        System.out.println(SquarePatch.squarePatch(0));
-//        ReverseAString.reverseAString("123 45 6");
+//        ReverseAString.reverseAString("HELLO world");
 
+        atBashCypher("A");
     }
 }
